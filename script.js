0482552d34945ec82ebffc8a48325f0d43537677
Wellington Gudes <wellcharmoso@gmail.com>
2024-01-10 18:55:54 -0300

    js da calc

---
+++ script.js
@@ -0,0 +1,17 @@
+num1 = parseFloat(prompt("Digite o primeiro número:"))
+operacao = prompt("Escolha umas das operações abaixo que deseja utilizar: \n [1] - Soma \n [2] - Subtração \n [3] - Multiplicação \n [4] - Divisão \n")
+num2 = parseFloat(prompt("Escolha o segundo número:"))
+
+if (operacao == "1"){
+    alert(`A soma de ${num1} + ${num2} é igual a ${num1 + num2}`)
+
+}else if (operacao == "2"){
+    alert(`A subtração de ${num1} - ${num2} é igual a ${num1 - num2}`)
+
+}else if (operacao == "3"){
+    alert(`A multiplicação de ${num1} x ${num2} é igual a ${num1 * num2}`)
+
+}else if (operacao == "4"){
+    alert(`A divisão de ${num1} + ${num2} é igual a ${num1 / num2}`)
+
+}
